835b8bcbc9e4bd065018ab936e060966a9c03709
박찬우 <cksdngh@gmail.com>
2020-08-10 23:18:50 +0900

    Updates

---
+++ precache-manifest.78c1d1ea18eb728b0d83e54d77130451.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc723a920efb1891532fcb2cbd46f75",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "45763015aec390765e92",
+    "url": "/movie_app_2020/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1a77ac92d440b33273b4",
+    "url": "/movie_app_2020/static/js/2.55a2858a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.55a2858a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45763015aec390765e92",
+    "url": "/movie_app_2020/static/js/main.28694512.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
